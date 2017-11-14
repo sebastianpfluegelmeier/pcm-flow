8603233c021df1c9bf893473e82dd65e0196f7e9
sebastian pfluegelmeier <pfluegelmeier.s@gmx.at>
2017-11-14 15:57:45 +0100

    Add cyclic check

--- src/graph.rs
+++ src/graph.rs
@@ -3,9 +3,11 @@ extern crate sample;
 use node::Node;
 use self::sample::Frame;
 use std::marker::PhantomData;
+use std::collections::HashMap;
 
 pub struct Graph<F: Frame, N: Node<F>> {
     nodes: Vec<Box<N>>,
+    // input_node, input_port, output_node, output_port
     connections: Vec<(usize, usize, usize, usize)>,
     _marker: PhantomData<F>,
 }
@@ -25,6 +27,12 @@ impl<F, N> Graph<F, N>
                          input_port: usize, 
                          output_node: usize, 
                          output_port: usize) -> Result<usize, String> {
+        self.connections.push((input_node, input_port, output_node, output_port));
+        if self.check_for_cycle(input_node, &mut HashMap::new()) {
+            return Err(format!("Found a Cycle when trying to commect node {} and node {}",
+                       input_node, output_node));
+        }
+        self.connections.pop();
         let input_ports  = (*self.nodes[input_node]).inputs_amt();
         let output_ports = self.nodes[input_node].outputs_amt();
         if input_port < input_ports && output_port < output_ports {
@@ -36,5 +44,26 @@ impl<F, N> Graph<F, N>
         } else {
             return Err(format!("Port {} does not exist on Node {}", output_port, output_port));
         }
+    }
+
+    fn check_for_cycle(&self,
+                       start: usize,
+                       mut visited: &mut HashMap<usize, ()> ) -> bool {
+        let mut neighbours = Vec::new(); 
+        for c in &self.connections {
+            if c.0 == start {
+                neighbours.push(c.2)
+            }
+        }
+        visited.insert(start, ());
+        for n in &neighbours {
+            if visited.contains_key(&n) {
+                return false;
+            }
+        }
+        for n in neighbours {
+            self.check_for_cycle(n, &mut visited);
+        }
+        return true;
     }
 }
