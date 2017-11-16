add996c7e1639f2fe60ca761350d4b923d782909
sebastian pfluegelmeier <pfluegelmeier.s@gmx.at>
2017-11-16 23:36:29 +0100

    Run Rustfmt

--- src/graph.rs
+++ src/graph.rs
@@ -1,10 +1,13 @@
-extern crate sample;
 extern crate petgraph;
+extern crate sample;
 
 use node::Node;
 use self::sample::Frame;
 use std::collections::HashMap;
 use self::petgraph::graph::Graph as PetGraph;
+
+type Connection = (PortId, PortId);
+type PortId = (usize, usize);
 
 pub struct Graph<F: Frame> {
     nodes: Vec<Box<Node<F>>>,
@@ -13,18 +16,19 @@ pub struct Graph<F: Frame> {
     input_buffers: Vec<Vec<F>>,
     output_buffers: Vec<Vec<F>>,
     // input_node, input_port, output_node, output_port
-    connections: Vec<(usize, usize, usize, usize)>,
+    connections: Vec<Connection>,
     // input_port<(node, port)>
-    input_connections: Vec<(usize, usize)>,
+    input_connections: Vec<PortId>,
     // output_port<(node, port)>
-    output_connections: Vec<(usize, usize)>,
-    outputs: HashMap<(usize, usize), (usize, usize)>,
+    output_connections: Vec<PortId>,
+    outputs: HashMap<PortId, PortId>,
     topological_sorting: Vec<usize>,
 }
 
-impl<F> Graph<F> 
-    where F: Frame {
-
+impl<F> Graph<F>
+where
+    F: Frame,
+{
     pub fn new() -> Self {
         Graph {
             nodes: Vec::new(),
@@ -36,25 +40,27 @@ impl<F> Graph<F>
             outputs: HashMap::new(),
             topological_sorting: Vec::new(),
             input_buffers: Vec::new(),
-            output_buffers: Vec::new()
+            output_buffers: Vec::new(),
         }
     }
 
     /// Add a new node to the Graph. Its ID gets returned.
     pub fn add_node(&mut self, node: Box<Node<F>>) -> usize {
         let index = self.nodes.len();
-        self.input_buffers .push(vec![F::equilibrium(); node.inputs_amt() ]);
-        self.output_buffers.push(vec![F::equilibrium(); node.outputs_amt()]);
+        self.input_buffers
+            .push(vec![F::equilibrium(); node.inputs_amt()]);
+        self.output_buffers
+            .push(vec![F::equilibrium(); node.outputs_amt()]);
         self.nodes.push(node);
         return index;
     }
 
-    pub fn connect_input(&mut self, input: usize, node: usize, port: usize) {
-        self.input_connections[input] = (node, port);
+    pub fn connect_input(&mut self, input: usize, port: PortId) {
+        self.input_connections[input] = port;
     }
 
-    pub fn connect_output(&mut self, output: usize, node: usize, port: usize) {
-        self.output_connections[output] = (node, port);
+    pub fn connect_output(&mut self, output: usize, port: PortId) {
+        self.output_connections[output] = port;
     }
 
     pub fn set_input_amt(&mut self, inputs: usize) {
@@ -67,41 +73,55 @@ impl<F> Graph<F>
         self.output_connections = vec![(0, 0); outputs];
     }
 
-    pub fn add_connection(&mut self,
-                         input_node: usize, 
-                         input_port: usize, 
-                         output_node: usize, 
-                         output_port: usize) -> Result<usize, String> {
-
-        let input_ports_amt  = (*self.nodes[input_node]).inputs_amt();
+    pub fn add_connection(
+        &mut self,
+        input_node: usize,
+        input_port: usize,
+        output_node: usize,
+        output_port: usize,
+    ) -> Result<usize, String> {
+        let input_ports_amt = (*self.nodes[input_node]).inputs_amt();
         let output_ports_amt = self.nodes[input_node].outputs_amt();
 
-        self.connections.push((input_node, input_port, output_node, output_port));
+        self.connections
+            .push(((input_node, input_port), (output_node, output_port)));
         if let Some(topo_sorting) = self.get_topological_sorting() {
             self.topological_sorting = topo_sorting;
         } else {
             self.connections.pop();
-            return Err(format!("Cycle found after Connecting Node {} and Node {}", 
-                       input_node, output_node));
+            return Err(format!(
+                "Cycle found after Connecting Node {} and Node {}",
+                input_node,
+                output_node
+            ));
         }
 
         if input_port < input_ports_amt && output_port < output_ports_amt {
             let connections_len = self.connections.len();
-            self.outputs.insert((input_node, input_port), (output_node, output_port));
+            self.outputs
+                .insert((input_node, input_port), (output_node, output_port));
             return Ok(connections_len);
         } else if input_port < input_ports_amt {
             self.connections.pop();
-            return Err(format!("Port {} does not exist on Node {}", input_port, input_node));
+            return Err(format!(
+                "Port {} does not exist on Node {}",
+                input_port,
+                input_node
+            ));
         } else {
             self.connections.pop();
-            return Err(format!("Port {} does not exist on Node {}", output_port, output_port));
+            return Err(format!(
+                "Port {} does not exist on Node {}",
+                output_port,
+                output_port
+            ));
         }
     }
 
     fn process_graph(&mut self) {
         // clear input and output buffers
         for i in 0..self.nodes.len() {
-            self.input_buffers[i]  = vec![F::equilibrium(); self.nodes[i].inputs_amt()];
+            self.input_buffers[i] = vec![F::equilibrium(); self.nodes[i].inputs_amt()];
             self.output_buffers[i] = vec![F::equilibrium(); self.nodes[i].outputs_amt()];
         }
 
@@ -112,12 +132,14 @@ impl<F> Graph<F>
 
         // go through the sorted nodes and pass the Frames on
         for node in &self.topological_sorting {
-            self.nodes[*node].process(&mut self.input_buffers[*node], 
-                                      &mut self.output_buffers[*node]);
+            self.nodes[*node].process(
+                &mut self.input_buffers[*node],
+                &mut self.output_buffers[*node],
+            );
             for output in 0..self.nodes[*node].outputs_amt() {
                 if let Some(&(input_node, input_port)) = self.outputs.get(&(*node, output)) {
-                    self.input_buffers[input_node][input_port] = 
-                        self.output_buffers[*node][output];
+                    self.input_buffers[input_node][input_port] 
+                        = self.output_buffers[*node][output];
                 }
             }
         }
@@ -138,7 +160,7 @@ impl<F> Graph<F>
             pet_ix_to_graph_ix.insert(petgraph_index, i);
         }
 
-        for &(s, _, e, _) in &self.connections {
+        for &((s, _), (e, _)) in &self.connections {
             petgraph.add_edge(graph_ix_to_pet_ix[&s], graph_ix_to_pet_ix[&e], ());
         }
 
@@ -149,14 +171,16 @@ impl<F> Graph<F>
                     result.push(pet_ix_to_graph_ix[&s]);
                 }
                 return Some(result);
-            },
-            Err(_) => {return None}
+            }
+            Err(_) => return None,
         }
     }
 }
 
-impl<F> Node<F> for Graph<F> where F: Frame {
-
+impl<F> Node<F> for Graph<F>
+where
+    F: Frame,
+{
     fn process(&mut self, inputs: &mut Vec<F>, outputs: &mut Vec<F>) {
         for i in 0..inputs.len() {
             self.graph_input_buffers[i] = inputs[i];
