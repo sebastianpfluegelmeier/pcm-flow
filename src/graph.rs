9eea1bcce5383dd1eed2f46f48561833f314e5e5
sebastian pfluegelmeier <pfluegelmeier.s@gmx.at>
2017-11-17 14:01:10 +0100

    Add documentation to graph.rs and add some TODOs

--- src/graph.rs
+++ src/graph.rs
@@ -9,19 +9,38 @@ use self::petgraph::graph::Graph as PetGraph;
 type Connection = (PortId, PortId);
 type PortId = (usize, usize);
 
+/// The main container struct for Processors.
+/// Processors can be added and connected in arbitrary 
+/// ways as long there are no cyclic connections.
+/// All Processors must be from the same Frame type.
+/// A graph has an arbitrary number of inputs and outputs
+/// which can be connected to processors.
+/// These inputs and outputs are called graph inputs and graph outputs.
 pub struct Graph<F: Frame> {
+    // contains all processors
     processors: Vec<Box<Processor<F>>>,
+    // buffers that contains the graph inputs
     graph_input_buffers: Vec<F>,
+    // buffers that contain the graph outputs
     graph_output_buffers: Vec<F>,
+    // input buffers for all processors
     input_buffers: Vec<Vec<F>>,
+    // output buffers for all processors
     output_buffers: Vec<Vec<F>>,
-    // input_processor, input_port, output_processor, output_port
+    // a list of all connections between the processors
+    // TODO: remove and use outputs instead
     connections: Vec<Connection>,
-    // input_port<(processor, port)>
-    input_connections: Vec<PortId>,
-    // output_port<(processor, port)>
-    output_connections: Vec<PortId>,
+    // a hash map describing all connections from port to port
+    // TODO: change to HashMap<PortId, HashSet<PortId>>
     outputs: HashMap<PortId, PortId>,
+    // a list of connections from the inputs to nodes
+    // TODO: change to HashMap<usize, HashSet<PortId>>
+    input_connections: Vec<PortId>,
+    // a list of connections from nodes to the outputs
+    // TODO: change to HashMap<usize, HashSet<PortId>>
+    output_connections: Vec<PortId>,
+
+    // stores all processor indexes sorted topologically
     topological_sorting: Vec<usize>,
 }
 
@@ -29,6 +48,7 @@ impl<F> Graph<F>
 where
     F: Frame,
 {
+    /// Create a new empty Graph
     pub fn new() -> Self {
         Graph {
             processors: Vec::new(),
@@ -55,28 +75,37 @@ where
         return index;
     }
 
+    /// Connect an input to a processor.
     pub fn connect_input(&mut self, input: usize, port: PortId) {
         self.input_connections[input] = port;
     }
 
+    /// connect an output to a processor.
     pub fn connect_output(&mut self, output: usize, port: PortId) {
         self.output_connections[output] = port;
     }
 
+    /// set the amount of inputs
     pub fn set_input_amt(&mut self, inputs: usize) {
         self.graph_input_buffers = vec![F::equilibrium(); inputs];
         self.input_connections = vec![(0, 0); inputs];
     }
 
+    /// set the amount of outputs
     pub fn set_output_amt(&mut self, outputs: usize) {
         self.graph_output_buffers = vec![F::equilibrium(); outputs];
         self.output_connections = vec![(0, 0); outputs];
     }
 
+    /// add aconnection between two ports
+    /// either returns an Ok(connection Id) or in case of a cycle or an invalid
+    /// port, a Err(Description)
     pub fn add_connection(
         &mut self,
+        // TODO: change to PortId
         input_processor: usize,
         input_port: usize,
+        // TODO: change to PortId
         output_processor: usize,
         output_port: usize,
     ) -> Result<usize, String> {
@@ -118,6 +147,7 @@ where
         }
     }
 
+    /// Values get passed along in the graph.
     fn process_graph(&mut self) {
         // clear input and output buffers
         for i in 0..self.processors.len() {
@@ -150,6 +180,7 @@ where
         }
     }
 
+    /// returns the topological sorting of the graph in case there is no cycle
     pub fn get_topological_sorting(&self) -> Option<Vec<usize>> {
         let mut petgraph: PetGraph<(), (), petgraph::Directed, u32> = PetGraph::new();
         let mut pet_ix_to_graph_ix = HashMap::new();
@@ -181,6 +212,8 @@ impl<F> Processor<F> for Graph<F>
 where
     F: Frame,
 {
+    /// takes an list of input Frames and output Frames, 
+    /// processes the input and writes it to the outputs list.
     fn process(&mut self, inputs: &mut Vec<F>, outputs: &mut Vec<F>) {
         for i in 0..inputs.len() {
             self.graph_input_buffers[i] = inputs[i];
@@ -191,11 +224,13 @@ where
         }
     }
 
+    /// returns the amount of inputs
     fn inputs_amt(&self) -> usize {
-        0
+        self.input_buffers.len()
     }
 
+    /// returns the amount of outputs
     fn outputs_amt(&self) -> usize {
-        0
+        self.input_buffers.len()
     }
 }
